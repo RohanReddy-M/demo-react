3ae45a1676a81f817c2b8734d4a52abd469dd422
RohanReddy-M <machireddy23@gmail.com>
2025-07-20 22:39:54 +0530

    small changes in app

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          github workflow example <code>src/App.js</code> by Rohan.
           <h1>Hello from Rohan!</h1>
         </p>
         <a
